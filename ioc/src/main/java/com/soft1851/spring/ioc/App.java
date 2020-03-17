b93efd7eb4a4036a5bc3983b3c0b478db90bbf79
czzeng-code <czzeng_ok@qq.com>
2020-03-17 17:48:01 +0800

    基于注解的配置类的用法（main和单元测试）

--- ioc/src/main/java/com/soft1851/spring/ioc/App.java
+++ ioc/src/main/java/com/soft1851/spring/ioc/App.java
@@ -1,5 +1,9 @@
 package com.soft1851.spring.ioc;
 
+import com.soft1851.spring.ioc.config.AppConfig;
+import com.soft1851.spring.ioc.entity.Book;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * @author Zeng
  * @ClassName App
@@ -9,6 +13,15 @@ package com.soft1851.spring.ioc;
  **/
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+
+        //生成基于注解配置的应用上下文对象
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        //手动开启扫包
+        ctx.scan("com.soft1851.spring.ioc.config");
+        //        ctx.refresh();
+        Book book = (Book) ctx.getBean("book");
+        book.setName("java");
+        book.setPrice(99.8);
+        System.out.println(book);
     }
 }
---
+++ ioc/src/main/java/com/soft1851/spring/ioc/entity/Book.java
@@ -0,0 +1,16 @@
+package com.soft1851.spring.ioc.entity;
+
+import lombok.Data;
+
+/**
+ * @author Zeng
+ * @ClassName Book
+ * @Description TOOD
+ * @Date 2020/3/17
+ * @Version 1.0
+ **/
+@Data
+public class Book {
+    private String name;
+    private Double price;
+}
